175363db6d674cdff476054587a063ab8fb03268
FabianoCarregas <fabianofac_@hotmail.com>
2021-12-07 10:29:52 -0300

    getAll drivers

--- src/main/java/com/fabiano/moobi/Interfaces/DriverAPI.java
+++ src/main/java/com/fabiano/moobi/Interfaces/DriverAPI.java
@@ -1,8 +1,12 @@
 package com.fabiano.moobi.Interfaces;
 
 import com.fabiano.moobi.Domain.Driver;
+import com.fabiano.moobi.Repositories.DriverRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,12 +14,15 @@ import java.util.List;
 
 @Service
 @RestController
+@RequestMapping(produces = MediaType.APPLICATION_JSON_VALUE)
 public class DriverAPI {
 
-    @GetMapping
+    @Autowired
+    DriverRepository driverRepository;
+
+    @GetMapping("/drivers")
     public List<Driver> listDrivers() {
-        return new ArrayList<>();
+        return driverRepository.findAll();
     }
 
-    
 }
